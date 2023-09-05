059c446aaac0c24966413b92acd9eb144a58abc5
KimDuHwan <21801010@handong.ac.kr>
2023-09-05 11:11:09 +0900

    Create and Read

--- src/WD/WordManager.java
+++ src/WD/WordManager.java
@@ -3,7 +3,14 @@ package WD;
 import java.util.Scanner;
 
 public class WordManager {
+    WordCRUD wordCrud;
     Scanner s = new Scanner(System.in);
+    WordManager(){
+        wordCrud = new WordCRUD(s);
+    }
+
+
+
     public int selectMenu(){
         System.out.print("*** 영단어 마스터 ***\n"
                 +"************************\n"
@@ -23,8 +30,33 @@ public class WordManager {
     public void start(){
         while(true) {
             int menu = selectMenu();
-            System.out.print(menu);
-            if(menu == 0 ) break;
+
+            if(menu == 0) break;//나가기
+            switch(menu){
+                case 1 ->{//모든 단어 보기
+                    wordCrud.listAll();
+                    break;
+                }
+                case 2 ->{//수준별 단어 보기
+                    break;
+                }
+                case 3 ->{//단어 검색
+                    break;
+                }
+                case 4 ->{//단어 추가
+                    wordCrud.addWord();
+                    break;
+                }
+                case 5 ->{//단어 수정
+                    break;
+                }
+                case 6 ->{//단어 삭제
+                    break;
+                }
+                case 7 ->{//파일 저장
+                    break;
+                }
+            }
         }
     }
 }
